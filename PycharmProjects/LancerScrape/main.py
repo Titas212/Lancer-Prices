6e349284c78cc0d936fb31ceed0f963b69f24c3b
Titas212 <titas2001@gmail.com>
2022-09-02 16:09:50 +0300

    It is now possible to calculate average price of the model. Also, the files are now written is subdir and their names are the date of script running. Furthermore, I wrote a method which cleans the subdir, if the content of the files is the same.

--- PycharmProjects/LancerScrape/main.py
+++ PycharmProjects/LancerScrape/main.py
@@ -1,16 +1,34 @@
+import dateutil.utils
 from bs4 import BeautifulSoup
 import requests
+import os.path
+import filecmp
 
 
 class Scrape:
     def __init__(self, url):
+        self.data_path = "/home/titas01/Lancer-Prices/PycharmProjects/LancerScrape/data"
+        self.dir_list = os.listdir(self.data_path)
+        self.completeName = os.path.join(self.data_path, str(dateutil.utils.today()) + ".txt")
         self.url = url
         self.page = requests.get(url)
         self.soup = BeautifulSoup(self.page.content, 'html.parser')
         self.prices_list = []
         self.names_list = []
         self.pages = 0
-        self.file = open("prices.txt", "a")
+        self.file = open(self.completeName, "a")
+
+    def clean_dir(self):
+        for i in range(len(self.dir_list)-1):
+            f1 = os.path.join(self.data_path, self.dir_list[i])
+            for j in range(len(self.dir_list)-1):
+                f2 = os.path.join(self.data_path, self.dir_list[j])
+                if filecmp.cmp(f1, f2) and i != j:
+                    os.remove(f2)
+                    self.dir_list.remove(self.dir_list[j])
+
+    def close_file(self):
+        self.file.close()
 
     def find_pages(self):
         total_pages = self.soup.findAll("li", {"class": "pagination-item"})
@@ -37,8 +55,27 @@ class Scrape:
             self.file.write("\n")
 
 
+class Calculations:
+
+    def __init__(self, names, prices):
+        self.names = names
+        self.prices = prices
+        self.num_prices = None
+
+    def price_to_number(self):
+        num_prices = []
+        for price in self.prices:
+            price_parsed = price.replace(",", "")
+            num_prices.append(float("".join(filter(str.isdigit, price_parsed))))
+        self.num_prices = num_prices
+
+    def average_price(self):
+        return sum(self.num_prices) / len(self.num_prices)
+
+
 scraper = Scrape("https://www.autoscout24.com/lst/mitsubishi/lancer/ve_evo?atype=C&page=1")
 scraper.find_pages()
 scraper.find_prices_names()
-
 scraper.write_to_file()
+scraper.close_file()
+scraper.clean_dir()
